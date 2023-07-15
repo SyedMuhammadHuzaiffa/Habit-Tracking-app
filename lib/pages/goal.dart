18f3ee6bb32031de2276923b5acfccc139862b00
Ibrahiim-Ahmed <ibrahimahmed3647@gmail.com>
2023-07-15 17:08:55 +0500

    Adding pages and setting navigating routes b/w 'em

---
+++ lib/pages/goal.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Goal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Text("Goal Page"),
+      ),
+    );
+  }
+}
---
+++ lib/pages/qoutes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Qoutes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Text("Qoutes Page"),
+      ),
+    );
+  }
+}
