5467501f54e4005021dc3f71d96bb10a8ca6d9e6
Ibrahiim-Ahmed <ibrahimahmed3647@gmail.com>
2023-07-21 14:27:22 +0500

    Simple Motivation page added

--- lib/pages/qoutes.dart
+++ lib/pages/qoutes.dart
@@ -1,13 +1,142 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/resources/qoutesfile.dart';
+
+void main() => runApp(const Qoutes());
 
 class Qoutes extends StatelessWidget {
+  const Qoutes({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      body: Center(
-        child: Text("Qoutes Page"),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Quotes App',
+      // theme: ThemeData(
+      //   primaryColor: Colors.black,
+      // ),
+      home: const MyHomePage(title: 'Quotes App'),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  void _changeQuote(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final quote = Quote.quotes[_currentIndex];
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            widget.title,
+            style: TextStyle(fontSize: 30),
+          ),
+          backgroundColor: Colors.black,
+        ),
+        backgroundColor: Color.fromARGB(255, 4, 107, 7),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  border: Border.all(color: Colors.black, width: 8),
+                ),
+                child: Text(
+                  'Quotes Categories',
+                  style: TextStyle(
+                    fontSize: 45,
+                    decoration: TextDecoration.underline,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  'Fashion',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+                onTap: () {
+                  _changeQuote(0);
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text(
+                  'Sports',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+                onTap: () {
+                  _changeQuote(1);
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text(
+                  "Inspirational",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+                onTap: () {
+                  _changeQuote(2);
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          ),
+        ),
+        body: GestureDetector(
+          onTap: () => _changeQuote((_currentIndex + 1) % Quote.quotes.length),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  quote.text,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                const SizedBox(height: 18),
+                Text(
+                  quote.author,
+                  style: const TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.yellow,
+                    fontStyle: FontStyle.italic,
+                    decoration: TextDecoration.underline,
+                    decorationColor: Color.fromARGB(255, 185, 21, 9),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
---
+++ lib/resources/qoutesfile.dart
@@ -0,0 +1,52 @@
+class Quote {
+  final String text;
+  final String author;
+
+  const Quote({required this.text, required this.author});
+
+  static List<Quote> quotes = [
+    Quote(
+      text: "You can never be overdressed or overeducated",
+      author: "-Oscar Wilde",
+    ),
+    Quote(
+      text: "Everybody has a plan until they get punched in face",
+      author: "Mike Tyson",
+    ),
+    Quote(
+      text: "I have not failed. I've just found 10,000 ways that won't work",
+      author: "Thomas Edison",
+    ),
+    Quote(
+      text:
+          "Fashion is not something that exists in dresses only. Fashion is in the sky, in the street, fashion has to do with ideas, the way we live, what is happening.",
+      author: "Coco Chanel",
+    ),
+    Quote(
+      text:
+          "Success is not final, failure is not fatal: it is the courage to continue that counts",
+      author: "Winston Churchil",
+    ),
+    Quote(
+      text: "The only way to do great work is to love what you do.",
+      author: "Steve Jobs",
+    ),
+    Quote(
+      text: "Don't let the fear of striking out hold you back",
+      author: "Babe Ruth",
+    ),
+    Quote(
+      text: "Fashion is the armour to survive the reality of everyday life.",
+      author: "Bill Cunnigham",
+    ),
+    Quote(
+      text: "I am the greatest, I said that even before I knew I was.",
+      author: "M. Ali",
+    ),
+    Quote(
+      text:
+          "I have a theory that movies operate on the level of dreams, where you dream yourself.",
+      author: "Ford Coppola",
+    ),
+  ];
+}
